00fae4c9be6b2a08cd838dd14085c37a383d7146
HongEunbeen <vvvv980@gmail.com>
2021-06-30 19:27:40 +0900

    feat : 세션 기능 추가

---
+++ src/main/java/eun/example/book/config/auth/dto/SessionUser.java
@@ -0,0 +1,19 @@
+package eun.example.book.config.auth.dto;
+
+import eun.example.book.domain.user.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
