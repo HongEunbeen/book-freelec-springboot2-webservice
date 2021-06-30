7211efbdb4da960dab2e355fde1c3c484887bcf8
HongEunbeen <vvvv980@gmail.com>
2021-06-30 19:30:30 +0900

    fix : api에 세션 기능 추가

--- src/main/java/eun/example/book/web/IndexController.java
+++ src/main/java/eun/example/book/web/IndexController.java
@@ -1,5 +1,6 @@
 package eun.example.book.web;
 
+import eun.example.book.config.auth.dto.SessionUser;
 import eun.example.book.service.posts.PostsService;
 import eun.example.book.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsservice;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsservice.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
